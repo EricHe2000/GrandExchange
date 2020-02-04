64f66316b2ef6525a8935bee01db02169a542e21
Edward Cheng <ec8bhb@virginia.edu>
2020-02-04 11:57:36 -0500

    changed url

--- grandExchange/mysite/urls.py
+++ grandExchange/mysite/urls.py
@@ -16,11 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.shortcuts import render
+from . import home
 
-def homeView(request):
-	return render(request, 'home.html')
+# def homeView(request):
+# 	return render(request, 'home.html')
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',homeView),
+    path('',home.homeView),
 ]
 
