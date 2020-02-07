c1f0a1543f8fb912b6facdcadbb1917aabc756a0
Dustin Nguyen <ddn3aq@virginia.edu>
2020-02-06 20:35:24 -0500

    added basic api view structure

--- grandExchange/mysite/home.py
+++ grandExchange/mysite/home.py
@@ -2,3 +2,31 @@ from django.shortcuts import render
 
 def homeView(request):
 	return render(request, 'home.html')
+
+def createUser(request):
+	return render(request,'test.html')
+
+def createItem(request):
+	return render(request,'test.html')
+
+def getUser(request):
+	return render(request,'test.html')
+
+def getItem(request):
+	return render(request,'test.html')
+
+
+def updateUser(request):
+	return render(request,'test.html')
+
+
+def updateItem(request):
+	return render(request,'test.html')
+
+
+def deleteUser(request):
+	return render(request,'test.html')
+
+
+def deleteItem(request):
+	return render(request,'test.html')
--- grandExchange/mysite/urls.py
+++ grandExchange/mysite/urls.py
@@ -23,5 +23,18 @@ from . import home
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',home.homeView),
+
+    path('api/v1/user/create/', home.createUser),
+    path('api/v1/item/create/', home.createItem),
+
+    path('/api/v1/user/<int:userid>/', home.getUser),
+    path('/api/v1/item/<int:itemid>/', home.getItem),
+
+    path('/api/v1/user/<int:userid>/update', home.updateUser),
+    path('/api/v1/item/<int:itemid>/update', home.updateItem),
+
+    path('/api/v1/user/<int:userid>/delete', home.deleteUser),
+    path('/api/v1/item/<int:itemid>/delete', home.deleteItem)
+
 ]
 
