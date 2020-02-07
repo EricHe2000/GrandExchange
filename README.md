8d4b7632139e8ea502c2c24b52c9659a46286c0c
Dustin Nguyen <ddn3aq@virginia.edu>
2020-02-06 20:43:53 -0500

    added a read me

---
+++ README.md
@@ -0,0 +1,23 @@
+DOCUMENTATION:
+
+URL PATHS:
+
+CREATE user/item:
+
+api/v1/user/create/
+api/v1/item/create/
+
+GET Request:
+
+api/v1/user/<int:userid>/
+api/v1/item/<int:itemid>
+
+UPDATE:
+
+api/v1/user/<int:userid>/update
+api/v1/item/<int:itemid>/update
+
+DELETE:
+
+api/v1/user/<int:userid>/delete
+api/v1/item/<int:itemid>/delete
--- grandExchange/mysite/urls.py
+++ grandExchange/mysite/urls.py
@@ -27,14 +27,14 @@ urlpatterns = [
     path('api/v1/user/create/', home.createUser),
     path('api/v1/item/create/', home.createItem),
 
-    path('/api/v1/user/<int:userid>/', home.getUser),
-    path('/api/v1/item/<int:itemid>/', home.getItem),
+    path('api/v1/user/<int:userid>/', home.getUser),
+    path('api/v1/item/<int:itemid>/', home.getItem),
 
-    path('/api/v1/user/<int:userid>/update', home.updateUser),
-    path('/api/v1/item/<int:itemid>/update', home.updateItem),
+    path('api/v1/user/<int:userid>/update', home.updateUser),
+    path('api/v1/item/<int:itemid>/update', home.updateItem),
 
-    path('/api/v1/user/<int:userid>/delete', home.deleteUser),
-    path('/api/v1/item/<int:itemid>/delete', home.deleteItem)
+    path('api/v1/user/<int:userid>/delete', home.deleteUser),
+    path('api/v1/item/<int:itemid>/delete', home.deleteItem)
 
 ]
 
