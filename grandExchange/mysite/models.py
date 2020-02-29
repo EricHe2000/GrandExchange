7bd89cfb363bcbcae4b7e48072cb6e24cbda7ae9
Edward Cheng <ec8bhb@virginia.edu>
2020-02-29 15:38:56 -0500

    added number bought

--- grandExchange/mysite/models.py
+++ grandExchange/mysite/models.py
@@ -7,6 +7,7 @@ class Item(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField(max_length=150)
     price = models.DecimalField(max_digits=10, decimal_places=2)
+    numberBought = models.IntegerField()
 
 
 class User(models.Model):
