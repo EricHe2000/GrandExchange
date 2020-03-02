6e727fc55c4f78aba8294ff222cda8a836bc3256
Dustin Nguyen <ddn3aq@virginia.edu>
2020-03-02 12:00:36 -0500

    updated fixtures

--- grandExchange/mysite/models.py
+++ grandExchange/mysite/models.py
@@ -7,7 +7,7 @@ class Item(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField(max_length=150)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    numberBought = models.IntegerField()
+    numberBought = models.IntegerField(default=0)
 
 
 class User(models.Model):
