3a1720be26c279459a99b2a4d6de0274546e5c3c
Edward Cheng <ec8bhb@virginia.edu>
2020-03-02 18:32:31 -0500

    static images fixed

--- grandExchange_web/grandExchange_web/settings.py
+++ grandExchange_web/grandExchange_web/settings.py
@@ -121,11 +121,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+
+
 STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(PROJECT_DIR,'static')
-STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-                os.path.join(PROJECT_DIR,'staticfiles'), # if your static files folder is named "staticfiles"
+    os.path.join(PROJECT_DIR,'static'), # if your static files folder is named "staticfiles"
 )
--- grandExchange_web/web/urls.py
+++ grandExchange_web/web/urls.py
@@ -23,5 +23,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='home'),
     path('detail/<int:num>/', views.detail, name='detail')
-]
- + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
