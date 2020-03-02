c696572e386703986c85212650e8262bd78b135d
Eric He <eriche2000@gmail.com>
2020-03-02 18:34:49 -0500

    new fixtures

--- grandExchange_web/web/views.py
+++ grandExchange_web/web/views.py
@@ -8,9 +8,11 @@ import logging
 def index(request):
     req = urllib.request.Request('http://exp:8000/api/v1/item/hottestCheapestList')
     resp_json = urllib.request.urlopen(req).read().decode('utf-8')
-    resp = json.loads(resp_json)
-    resp_list = list(resp.values())
-    return render(request, 'test.html',context = {'dict':resp_list})
+    d = json.loads(resp_json)
+    dictCheap = [d["Cheapest1"], d["Cheapest2"],d["Cheapest3"]]
+    dictHot = [d["Hottest1"], d["Hottest2"],d["Hottest3"]]
+    #resp_list = list(resp.values())
+    return render(request, 'test.html',context = {'cheap':dictCheap,'hot':dictHot} )
 
 def detail(request,num=1):
 
