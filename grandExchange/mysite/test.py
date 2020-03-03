3611cb0a21862fdb3aa7af127fd6e397f7d80800
Dustin Nguyen <ddn3aq@virginia.edu>
2020-03-02 21:33:07 -0500

    4 more test cases!

--- grandExchange/mysite/test.py
+++ grandExchange/mysite/test.py
@@ -14,18 +14,92 @@ class GetItemTestCase(TestCase):
     def setUp(self):
         pass #nothing to set up
 
+    #checks to see if it returns the correct item
     def test_success_response(self):
-        response = self.client.get(reverse('getItem', kwargs={'itemid':4}))
-        print(response.json())
-        #self.assertContains(response, 'order_list')
-        self.assertEqual(1,1)
+        item_id = 4
+        response = self.client.get(reverse('getItem', kwargs={'itemid':item_id}))
+        value = response.json()
+        self.assertEqual(value['id'], item_id)
 
-    #user_id not given in url, so error
+    #returns good error message when no item is found
     def test_fails_invalid(self):
-        #response = self.client.get(reverse('all_orders_list'))
-        #self.assertEquals(response.status_code, 404)
-        self.assertEqual(1,1)
+        item_id = 999
+        response = self.client.get(reverse('getItem', kwargs={'itemid':item_id}))
+        value = response.json()
+        self.assertEqual(value['Error'], 'Item does not exist')
 
     #tearDown method is called after each test
     def tearDown(self):
         pass #nothing to tear down
+
+
+
+class GetHottestItemsTestCase(TestCase):
+    # Create your tests here.
+    # loading our fixture for testing
+    fixtures = ['fixtures.json']
+
+    # setUp method is called before each test in this class
+    def setUp(self):
+        pass  # nothing to set up
+
+    # checks to see if it returns the correct item
+    def test_success_response(self):
+        hot_item = "Rune scimitar"
+        response = self.client.get(reverse('getHottestItem'))
+        value = response.json()
+        self.assertEqual(value[0]['title'], hot_item)
+
+    # returns good error message when no item is found
+    def test_fails_invalid(self):
+        second_hot_item = "Yew logs"
+        response = self.client.get(reverse('getHottestItem'))
+        value = response.json()
+        self.assertEqual(value[1]['title'], second_hot_item)
+
+    # tearDown method is called after each test
+    def tearDown(self):
+        pass  # nothing to tear down
+
+
+class GetCheapestItemsTestCase(TestCase):
+    # Create your tests here.
+    # loading our fixture for testing
+    fixtures = ['fixtures.json']
+
+    # setUp method is called before each test in this class
+    def setUp(self):
+        pass  # nothing to set up
+
+    # checks to see if it returns the correct item
+    def test_success_response(self):
+        cheap_item = "12.00"
+        response = self.client.get(reverse('getCheapestItem'))
+        value = response.json()
+        self.assertEqual(value[0]['price'], cheap_item)
+
+    # tearDown method is called after each test
+    def tearDown(self):
+        pass  # nothing to tear down
+
+class DeleteItemTestCase(TestCase):
+    # Create your tests here.
+    # loading our fixture for testing
+    fixtures = ['fixtures.json']
+
+    # setUp method is called before each test in this class
+    def setUp(self):
+        pass  # nothing to set up
+
+    # checks to see if it returns the correct item
+    def test_success_response(self):
+
+        item_id = 2
+
+        response = self.client.get(reverse('deleteItem', kwargs={'itemid':item_id}))
+        value = response.json()
+        self.assertEqual(value['Success'], 'Your item has been deleted.')
+
+    # tearDown method is called after each test
+    def tearDown(self):
+        pass  # nothing to tear down
