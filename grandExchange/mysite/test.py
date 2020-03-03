84b3a5822fe21cc345ac2f5289f044b2169fe57c
Eric He <eriche2000@gmail.com>
2020-03-02 19:45:56 -0500

    new

---
+++ grandExchange/mysite/test.py
@@ -0,0 +1,12 @@
+from django.test import TestCase
+from django.urls import reverse
+from django.test import Client
+from .models import Item
+
+# Create your tests here.
+def hottestTest(self):
+    items = Item.objects.all()
+    
+
+
+     
