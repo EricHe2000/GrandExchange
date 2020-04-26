31d13367414bf9273c21d591693c184f7c7fc64c
He <eh4xv@virginia.edu>
2020-04-26 12:55:53 -0400

    tests 1

--- tests.py
+++ tests.py
@@ -1,5 +1,6 @@
 import unittest
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 class BasicWebTestCase(unittest.TestCase):
@@ -16,3 +17,17 @@ class BasicWebTestCase(unittest.TestCase):
         driver.get("http://web:8000")
         self.assertEqual(driver.title, "GrandExchange")
         self.assertFalse(driver.title == "Error lol")
+    
+    def login(self):
+        driver = self.driver
+        driver.get("http://web:8000/create/user")
+        driver.find_element_by_name('name').send_keys('Eric')
+        driver.find_element_by_name('email').send_keys('eriche2000@gmail.com')
+        driver.find_element_by_name('age').send_keys('21')
+        driver.find_element_by_name('username').send_keys('12345')
+        driver.find_element_by_name('password').send_keys('54321')
+        driver.implicitly_wait(20)
+        driver.find_element_by_name('username').send_keys('12345')
+        driver.find_element_by_name('password').send_keys('54321')
+        driver.get("http://web:8000/")
+        self.assertTrue("Logout" in driver.page_source)
