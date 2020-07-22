8a7df5d76cd4d66fa15df4d653f077749593b865
Eric He <eriche2000@gmail.com>
2020-07-22 15:58:15 -0400

    Update settings.py

--- grandExchange_exp/grandExchange_exp/settings.py
+++ grandExchange_exp/grandExchange_exp/settings.py
@@ -20,8 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '(so)^leko*j%#l4+p$z@twf+&ub+u@t-qmjnf45a7=jhnrt0_%'
-
+SECRET_KEY = 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
